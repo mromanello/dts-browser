8c44b7fdc2cdfbb2388ed2f19c42702e3320e432
MrGecko <vrlitwhai.kridanik@gmail.com>
2018-09-06 12:40:12 +0200

    add a full document view

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+import os
+
 import json
 import sys
-
-from flask import render_template, request, current_app
+import lxml.etree as ET
+import pprint
+from flask import render_template, request, current_app, Markup
 from urllib.request import build_opener
 
 from app import app_bp
@@ -13,7 +16,7 @@ else:
     json_loads = json.loads
 
 
-def query_json_endpoint(url, method='GET', headers_arg=None):
+def query_endpoint(url, method='GET', headers_arg=None):
     headers = {'Content-Type': 'application/ld+json'}
     if headers_arg is not None:
         headers.update(headers_arg)
@@ -25,7 +28,7 @@ def query_json_endpoint(url, method='GET', headers_arg=None):
     else:
         raise NotImplementedError
 
-    return json_loads(data)
+    return data
 
 
 def query_nautilus(request, endpoint):
@@ -35,8 +38,11 @@ def query_nautilus(request, endpoint):
     if len(request.args) > 0:
         params = "?" + "&".join(["%s=%s" % (k, v) for k, v in request.args.items()])
     url = base_url + endpoint_url + params
-    return url, query_json_endpoint(url)
+    return url, query_endpoint(url)
 
+
+xslt = ET.parse(os.getcwd() + '/app/static/xsl/tei2html.xsl')
+transform = ET.XSLT(xslt)
 
 """
     ROUTES
@@ -51,7 +57,7 @@ def index():
 @app_bp.route("/collections")
 def collections():
     api_url, collection = query_nautilus(request, "collections")
-    return render_template("main/collection.html", collection=collection, api_url=api_url)
+    return render_template("main/collection.html", collection=json_loads(collection), api_url=api_url)
 
 
 @app_bp.route("/navigation")
@@ -61,4 +67,10 @@ def navigation():
 
 @app_bp.route("/document")
 def document():
-    return render_template("main/document.html")
+    api_url, doc = query_nautilus(request, "document")
+
+    dom = ET.fromstring(doc)
+    newdom = transform(dom)
+    center_div = newdom.xpath("//*[name()='div' and @id='center']")[0]
+    safe_dom = Markup(ET.tostring(center_div).decode('utf-8'))
+    return render_template("main/document.html", document=safe_dom, api_url=api_url)
